e16ae2182af82fc0287736ba0ff3bf8a4867ebce
j4m3s <james15102547@gmail.com>
2025-09-07 21:41:53 +0700

    Update code in square_service_server.cpp

--- rclcpp/sec_b/cpp_service_server/square_service_server.cpp
+++ rclcpp/sec_b/cpp_service_server/square_service_server.cpp
@@ -145,7 +145,6 @@ private:
     double normalize_angle(double angle) {
         while (angle > M_PI) angle -= 2 * M_PI;
         while (angle < -M_PI) angle += 2 * M_PI;
-
         return angle;
     }
 
